5e721c9d77994bf2818b705437f7f340125b19eb
Robert Lowe <robertlowe15@gmail.com>
2017-08-13 21:58:29 +0100

    Config added

---
+++ config/config.php
@@ -0,0 +1,16 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+// SkyScanner API KEY
+$sk_api_key = "ro272479499858557678555613499413";
+
+// Browse Cached Flights
+$sk_browser_url = "http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/GB/GBP/en-GB/";
+
+// Live Flight Price
+$sk_live_price_url = "http://partners.api.skyscanner.net/apiservices/pricing/v1.0/GB/GBP/en-GB/";
+
+?>
